a9dcb8b3b06fb264263159d308617a96c3739982
Marti <martifabrega@gmail.com>
2024-09-17 11:42:50 +0200

    Improve AI answer parsing function

--- server/src/common/utils.ts
+++ server/src/common/utils.ts
@@ -72,11 +72,30 @@ export const getTrainingData = (type: string): Question[] | ReadingQuestion[] =>
 }
 
 export const AIAnswerToObject = (text: string) => {
-  const json = JSON.parse(text);
-
-  if (json) {
+  try { // text correctly formatted as JSON
+    const json = JSON.parse(text);
     return json;
-  }
+  } catch (error) { // malformatted JSON -> try to parse with regex
+    try {
+      const answerIdxRegex = /"answerIdx": ([0-4])/; // matches 0 to 4 after "answerIdx"
+      const explanationRegex = /(?<="explanation": )[\s\S]*/;  // matches EVERYTHING after "explanation"
+      
+      const answerIdxMatch =  text.match(answerIdxRegex);
+      const explantionMatch = text.match(explanationRegex);
 
-  return null; // Handle cases where the format doesn't match
-}
+      if (answerIdxMatch && explantionMatch) {
+        const answerIdx = answerIdxMatch[1]; // Get capture group
+        const explanation = explantionMatch[0];
+
+        const json = { answerIdx, explanation };
+        return json;
+      } else {
+        console.error('Regex match failed');
+        return null;
+      }
+    } catch (finalError) {
+      console.error(finalError);
+      return null
+    }
+  }
+};
