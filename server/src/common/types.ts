ac753f9171ae68f5028e3e8054dae6e67034aaf3
Marti <martifabrega@gmail.com>
2024-09-18 14:57:33 +0200

    Adjust and add prisma types

--- server/src/common/types.ts
+++ server/src/common/types.ts
@@ -1,5 +1,24 @@
-export interface Question {
+
+// TRAIN DATA TYPES
+export interface TrainQuestion {
     id: number,
+    question: string,
+    answers: string[],
+    type: string,
+    correct: number,
+    explanation: string,
+    difficulty: string,
+    readingQuestionId?: number | null,
+}
+
+export interface TrainReadingQuestion {
+    id: number,
+    type: string,
+    text: string,
+    questions: TrainQuestion[],
+}
+
+export interface RequestTrainQuestion {
     question: string,
     answers: string[],
     type: string,
@@ -8,10 +27,45 @@ export interface Question {
     difficulty: string,
 }
 
+export interface RequestTrainReadingQuestion {
+    type: string,
+    text: string,
+    questions: TrainQuestion[],
+}
+
+// Union types
+export type RequestFullTrainQuestion = RequestTrainQuestion | RequestTrainReadingQuestion;
+
+export type ResponseFullTrainQuestion = TrainQuestion | TrainReadingQuestion;
+
+// 'APP' DATA TYPES
+export interface Question {
+    id: number,
+    question: string,
+    answers: string[],
+    difficulty?: string,
+    readingQuestionId?: number,
+}
+
 export interface ReadingQuestion {
     id: number,
-    type: string,
     text: string,
     questions: Question[],
 }
 
+
+export interface RequestQuestion {
+    question: string,
+    answers: string[],
+    difficulty?: string,
+}
+
+export interface RequestReadingQuestion {
+    text: string,
+    questions: Question[],
+}
+
+// Union types
+export type RequestFullQuestion = RequestQuestion | RequestReadingQuestion;
+
+export type ResponseFullQuestion = Question | ReadingQuestion;
