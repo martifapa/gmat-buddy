93e8256f0738216d7a67d94ab9fece0fec31cf0d
Marti <martifabrega@gmail.com>
2024-09-20 09:48:55 +0200

    Initial commit for splitting question types

--- client/src/common/types/question.ts
+++ client/src/common/types/question.ts
@@ -2,44 +2,23 @@ export interface Question {
     id: number,
     question: string,
     answers: string[],
-    type: string,
-    correct: string,
-    explanation: string,
-    difficulty: string,
+    difficulty?: string,
 }
+
+export interface ReadingQuestion {
+    id: number,
+    text: string,
+    questions: Question[],
+}
+
+export type FullQuestion = Question | ReadingQuestion;
 
 export interface QuestionState {
     status: string,
-    questionBank: Question[],
+    questionBank: FullQuestion[],
 }
 
 export interface AiAnswer {
     answerIdx: number,
     explanation: string,
 }
-
-
-// export interface Question {
-//     id: number,
-//     question: string,
-//     answers: string[],
-//     difficulty?: string,
-// }
-
-// export interface ReadingQuestion {
-//     id: number,
-//     text: string,
-//     questions: Question[],
-// }
-
-// export type FullQuestion = Question | ReadingQuestion;
-
-// export interface QuestionState {
-//     status: string,
-//     questionBank: FullQuestion[],
-// }
-
-// export interface AiAnswer {
-//     answerIdx: number,
-//     explanation: string,
-// }
