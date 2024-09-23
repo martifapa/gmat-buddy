9cb2d869286405366429ea4addab8f69ce4815a4
Marti <martifabrega@gmail.com>
2024-09-23 14:18:42 +0200

    Update tests to questionType split

--- server/src/__tests__/routes/question.test.ts
+++ server/src/__tests__/routes/question.test.ts
@@ -17,7 +17,7 @@ jest.mock('../../common/prisma', () => ({
 import prisma from '../../common/prisma';
 
 describe('GET /questions/all', () => {
-    it('should return a list of questions and reading-questions', async () => {
+    it('should return a list of (non-reading) questions', async () => {
         (prisma.question.findMany as jest.Mock).mockResolvedValue([
             { id: 1, question: 'question text', answers: ['answer 1'], type: 'test' }
         ]);
@@ -30,7 +30,44 @@ describe('GET /questions/all', () => {
         const response = await request(app).get('/question/all');
 
         expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(2); // Questions (1) + Reading-questions (1)
+        expect(response.body).toHaveLength(1); // Questions (1) + Reading-questions (0)
+        expect(response.body[0]).toEqual(
+            expect.objectContaining({
+                question: 'question text',
+            })
+        );
+    });
+
+    it('should return 500 if an error occurs', async () => {
+        (prisma.question.findMany as jest.Mock).mockRejectedValue(new Error('Prisma error'));
+
+        const response = await request(app).get('/question/all');
+
+        expect(response.status).toBe(500);
+        expect(response.body).toEqual({ error: 'Internal server error' });
+    });
+});
+
+describe('GET /questions/all/reading', () => {
+    it('should return a list of reading-questions', async () => {
+        (prisma.question.findMany as jest.Mock).mockResolvedValue([
+            { id: 1, question: 'question text', answers: ['answer 1'], type: 'test' }
+        ]);
+        (prisma.readingQuestion.findMany as jest.Mock).mockResolvedValue([
+            { id: 2, text: 'reading-question text', questions: [
+                { id: 1, question: 'question text', answers: ['answer 1'], type: 'test' }
+            ]}
+        ]);
+
+        const response = await request(app).get('/question/all/reading');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1); // Questions (1) + Reading-questions (0)
+        expect(response.body[0]).toEqual(
+            expect.objectContaining({
+                text: 'reading-question text',
+            })
+        );
     });
 
     it('should return 500 if an error occurs', async () => {
--- server/src/__tests__/utils/ddbb.test.ts
+++ server/src/__tests__/utils/ddbb.test.ts
@@ -91,7 +91,31 @@ describe('getQuestions', () => {
 
         const result = await getQuestions();
 
-        expect(result).toHaveLength(2); // Questions (1) +  Reading-questions (1)
+        expect(result).toHaveLength(1); // Questions (1) +  Reading-questions (0)
+    });
+
+    it('should handle empty data', async () => {
+        (prisma.question.findMany as jest.Mock).mockResolvedValue([]);
+        (prisma.readingQuestion.findMany as jest.Mock).mockResolvedValue([]);
+
+        const result = await getQuestions();
+
+        expect(result).toHaveLength(0); // Questions (0) + Reading-questions (0)
+    });
+});
+
+describe('getReadingQuestions', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should fetch question and reading-questions', async () => {
+        (prisma.question.findMany as jest.Mock).mockResolvedValue([{ id: 1, question: 'question text', answers: ['answer 1'], type: 'test' }]);
+        (prisma.readingQuestion.findMany as jest.Mock).mockResolvedValue([{ id: 2, text: 'reading-question text', questions: [{ id: 1, question: 'question text', answers: ['answer 1'], type: 'test' }] }]);
+
+        const result = await getQuestions();
+
+        expect(result).toHaveLength(1); // Questions (0) +  Reading-questions (1)
     });
 
     it('should handle empty data', async () => {
