245a8c0be20a01cbb5a5330478e3e86e6d4dcd86
Marti <martifabrega@gmail.com>
2024-09-24 09:56:16 +0200

    Pass onClick and value to props

--- client/src/modules/login/components/Login.tsx
+++ client/src/modules/login/components/Login.tsx
@@ -1,31 +1,45 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { SyntheticEvent } from 'react';
-import styles from '../styles/Login.module.css';
+import { SyntheticEvent, useState } from 'react';
+
 import Input from '../../../components/input/Input';
 import { useAppDispatch } from '../../../common/hooks/redux';
 import { fetchQuestions } from '../../../redux/slices/question';
 import { fetchReadingQuestions } from '../../../redux/slices/readingQuestion';
+import { login } from '../../../services/auth';
+import { saveToken } from '../../../redux/slices/auth';
+
+import styles from '../styles/Login.module.css';
+import { showToastMessage } from '../../../common/utils';
 
 
 export default function Login() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = (event: SyntheticEvent) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
-    console.log('submit');
-    // if login successful --> trigger fetchQuestions
-    dispatch(fetchQuestions());
-    dispatch(fetchReadingQuestions());
-    navigate('/');
+    const { token, error } = await login(username, password);
+    
+    if (token) { // login successful
+      dispatch(saveToken(token));
+      dispatch(fetchQuestions());
+      dispatch(fetchReadingQuestions());
+      navigate('/');
+    } else {
+      showToastMessage(error)
+    }
+    
   };
 
   return (
     <div className={styles['form-container']}>
         <h2 className={styles.title}>Welcome back!</h2>
         <form onSubmit={handleSubmit} className={styles['user-form']}>
-            <Input label='Email' type='text' />
-            <Input label='Password' type='password' />
+            <Input label='Username' type='text' value={username} setValue={setUsername} />
+            <Input label='Password' type='password' value={password} setValue={setPassword}  />
             <input type="submit" value="Login" />
         </form>
         <p>Not a user yet? Register <Link to="/register">here</Link></p>
