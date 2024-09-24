8057f1f61f383be2dfafd55c2b8820f0ae7704cc
Marti <martifabrega@gmail.com>
2024-09-24 11:49:06 +0200

    Manage response errors

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -3,10 +3,21 @@ import { BASE_URL } from "../common/constants";
 
 
 export const login = async (username: string, password: string) => {
-    const response = await axios.post(
-        `${BASE_URL}/user/login`,
-        { username, password },
-    );
-    
-    return response.data;
+    try {
+        const response = await axios.post(
+            `${BASE_URL}/user/login`,
+            { username, password },
+        );
+        return response.data; // Successful login return user data
+    } catch (error) {
+        if (axios.isAxiosError(error)) { // Request made and server responded
+            if (error.response) {
+                return { error: error.response.data.error };
+            } else {
+                return { error: 'An unexpected error occurred. Please try again' };
+            }
+        } else { // Request made and server didn't respond?
+            return { error: 'An unexpected error occurred. Please try again' };
+        }
+    }
 }
