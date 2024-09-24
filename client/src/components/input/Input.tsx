0eaa35616a0ba284f356a526697f566856da6acd
Marti <martifabrega@gmail.com>
2024-09-24 09:57:18 +0200

    Pass onChange and value to props

--- client/src/components/input/Input.tsx
+++ client/src/components/input/Input.tsx
@@ -1,13 +1,18 @@
 interface Props {
     label: string,
     type: string,
+    value: string,
+    setValue: (value: string) => void,
 }
 
-export default function Input({ label, type }: Props) {
+export default function Input({ label, type, value, setValue }: Props) {
   return (
         <label>
             { label }
-            <input type={ type } />
+            <input
+                type={ type }
+                value={value}
+                onChange={(e) => setValue(e.target.value)} />
         </label>
     )
 };
