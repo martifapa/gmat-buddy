7a1454f2f5b777226773ecd1b69c75c8ba339685
Marti <martifabrega@gmail.com>
2024-09-24 10:40:45 +0200

    Modify error and success hex colors

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -3,8 +3,12 @@ export const BASE_URL = 'http://localhost:3000';
 export const IDLE = 'idle';
 export const LOADING = 'loading';
 
-export const HIDDEN = 'hidden';
+// Toast constants
+export const HIDDEN = '';
 export const ACTIVE = 'active';
+export const FADE = 'fade';
+export const ERROR = 'error';
+export const SUCCESS = 'success';
 
 export const QUESTION_TYPES = ['Verbal Reasoning - Sentence Correction', 'Verbal Reasoning - Reading Comprehension', 'Verbal Reasoning - Critical Reasoning', 'Quantitative Reasoning - Data Sufficiency', 'Quantitative Reasoning - Problem Solving'];
 export const READING_TYPE = 'Verbal Reasoning - Reading Comprehension';
