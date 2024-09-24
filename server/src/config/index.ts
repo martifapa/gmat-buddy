d658c035aaf5a06561e08ea201dc2d0b5d790edd
Marti <martifabrega@gmail.com>
2024-09-24 10:02:56 +0200

    Add default SECRET_KEY

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 
-const { PORT, GROQ_API_KEY } = process.env;
+const { PORT, GROQ_API_KEY, SECRET_KEY='default_SECRET_KEY' } = process.env;
+
+if (SECRET_KEY === 'default_SECRET_KEY') {
+    throw new Error('SECRET_KEY is not defined');
+}
 
 
-export { PORT, GROQ_API_KEY };
+export { PORT, GROQ_API_KEY, SECRET_KEY };
