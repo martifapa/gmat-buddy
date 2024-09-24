e2e82d90f80bf94237fd488b598ccc777c1d48d8
Marti <martifabrega@gmail.com>
2024-09-24 13:35:32 +0200

    Use register function from custom hook

--- client/src/modules/register/components/Register.tsx
+++ client/src/modules/register/components/Register.tsx
@@ -1,24 +1,30 @@
-import { SyntheticEvent } from 'react';
 import Input from '../../../components/input/Input';
 
 import styles from '../styles/Register.module.css';
+import useAuth from '../../../common/hooks/useAuth';
 
 
 export default function Register() {
-
-    const handleSubmit = (event: SyntheticEvent) => {
-        event.preventDefault();
-        console.log('register');
-    }
+  const {
+    username,
+    email,
+    password,
+    password2,
+    setUsername,
+    setEmail,
+    setPassword,
+    setPassword2,
+    register,
+  } = useAuth();
     
   return (
     <div className={styles['form-container']}>
         <h2 className={styles.title}>Welcome aboard!</h2>
-        <form onSubmit={handleSubmit} className={styles['user-form']}>
-            <Input label='Username' type='text' />
-            <Input label='Email' type='email' />
-            <Input label='Password' type='password' />
-            <Input label='Repeat password' type='password' />
+        <form onSubmit={register} className={styles['user-form']}>
+            <Input label='Username' type='text' value={username} setValue={setUsername} />
+            <Input label='Email' type='text' value={email} setValue={setEmail} />
+            <Input label='Password' type='password' value={password} setValue={setPassword} />
+            <Input label='Repeat password' type='password' value={password2} setValue={setPassword2} />
             <input type="submit" value="Register" />
         </form>
     </div>
