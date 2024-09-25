bf11baa75f90acd2d048a3639fa8f80626175ae1
Marti <martifabrega@gmail.com>
2024-09-25 16:55:15 +0200

    Set input placeholders with redux data

--- client/src/modules/settings/components/Settings.tsx
+++ client/src/modules/settings/components/Settings.tsx
@@ -1,9 +1,12 @@
 import LabelWithIcon from './LabelWithIcon';
+import { useAppSelector } from '../../../common/hooks/redux';
 
 import styles from '../styles/Settings.module.css';
 
 
 export default function Settings() {
+  const { username, email } = useAppSelector(state => state.user);
+
   return (
     <div className={styles['settings-container']}>
         <h2 className={styles.title}>Settings</h2>
@@ -24,11 +27,11 @@ export default function Settings() {
               <h2 className={styles['section-title']}>Personal information</h2>
               <fieldset>
                 <label>Username</label>
-                <input type="text" value='username' />
+                <input type="text" value={username}/>
               </fieldset>
               <fieldset>
                 <label>Email:</label>
-                <input type="email" value='email@example.com'/>
+                <input type="email" value={email}/>
               </fieldset>
               <button>Save</button>
             </div>
@@ -36,7 +39,7 @@ export default function Settings() {
               <h2 className={styles['section-title']}>Security</h2>
               <fieldset>
                 <label>Password:</label>
-                <input type="password" value='secret'/>
+                <input type="password"/>
                 <button>Change password</button>
               </fieldset>
             </div>
