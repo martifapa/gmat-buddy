0141f9ca84ca1220e777f5e0601026e249437446
Marti <martifabrega@gmail.com>
2024-09-26 15:38:06 +0200

    Show navlink if authenticated

--- client/src/modules/navBar/components/Navbar.tsx
+++ client/src/modules/navBar/components/Navbar.tsx
@@ -14,36 +14,39 @@ const Navbar = () => {
 
     return (<div className={style.navbar}>
         <p className={style.logo}><Link to="/"><span>GMAT</span> assistant</Link></p>
-        <ul className={style.navigation}>
-            <li><Link to="/">List</Link></li>
-            <li><Link to="custom">Custom</Link></li>
-            <li className={style.profile} onClick={toggle}>
-                <img src="/avatar.svg" alt="Avatar icon" className={style['profile-icon']} />
-                {toggleState && (
-                    <div className={style['profile-dropdown']}>
-                        <div className={style['dropdown-content']}>
-                            {
-                                isAuthenticated
-                                ? <DropdownOption
-                                    route="/login"
-                                    imgSrc="/logout.svg"
-                                    alt="Log out icon"
-                                    label="Log out"
-                                    onClick={logout}
-                                />
-                                : <DropdownOption
-                                    route="/login"
-                                    imgSrc="/login.svg"
-                                    alt="Login icon"
-                                    label="Login"
-                                /> 
-                            }
-                            <DropdownOption route="/settings" imgSrc="/settings.svg" alt="Settings icon" label="Settings"/>
+        {
+            isAuthenticated &&
+            <ul className={style.navigation}>
+                <li><Link to="/">List</Link></li>
+                <li><Link to="custom">Custom</Link></li>
+                <li className={style.profile} onClick={toggle}>
+                    <img src="/avatar.svg" alt="Avatar icon" className={style['profile-icon']} />
+                    {toggleState && (
+                        <div className={style['profile-dropdown']}>
+                            <div className={style['dropdown-content']}>
+                                {
+                                    isAuthenticated
+                                    ? <DropdownOption
+                                        route="/login"
+                                        imgSrc="/logout.svg"
+                                        alt="Log out icon"
+                                        label="Log out"
+                                        onClick={logout}
+                                    />
+                                    : <DropdownOption
+                                        route="/login"
+                                        imgSrc="/login.svg"
+                                        alt="Login icon"
+                                        label="Login"
+                                    /> 
+                                }
+                                <DropdownOption route="/settings" imgSrc="/settings.svg" alt="Settings icon" label="Settings"/>
+                            </div>
                         </div>
-                    </div>
-                )}
-            </li>
-        </ul>
+                    )}
+                </li>
+            </ul>
+        }
     </div>);
 };
 
