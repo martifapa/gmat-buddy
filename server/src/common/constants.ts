49036d805fa3a5d0b1b81ff23caaeb853c3c2baa
Marti <martifabrega@gmail.com>
2024-09-26 15:04:58 +0200

    Tweak prompts to ensure stable otuput

--- server/src/common/constants.ts
+++ server/src/common/constants.ts
@@ -10,15 +10,16 @@ export const getQuestionTypePrompt = `
 export const solveQuestionPrompt = `
     ${basePrompt}
     Given the provided question
-    (1) Provide the index of the correct answer (0 to 4)
+    (1) provide the index of the correct answer (0 to 4)
     (2) and explain clearly why it is the correct answer.
 
     Return ONLY a JSON object with open and closing braces ("{}") and with two keys: answerIdx, explanation. Your answer should only provide the JSON object, nothing outside from it. Here is an example of hot NOT TO respond, because it includes something else appart from the JSON object:
     "Here is the answer:
     {
         "answerIdx": 0,
-        "explanation": "This is a placeholder explanation"
+        "explanation": "placeholder explanation"
     }"
+    Do not mention the index of any index in your explanation.
 
     Use the following examples for reference to provide the explanation:`;
 
@@ -27,8 +28,12 @@ export const askDifferentExplanation = `
     Explain, clearly and with a different approach than the one the provided explanation takes, the provided GMAT question.
     
 
-    STRICTLY follow this pattern to answer the question:
-    Explanation: <explanation>
+    Return ONLY a JSON object with open and closing braces ("{}") and with one key: , explanation. Your answer should only provide the JSON object, nothing outside from it. Here is an example of hot NOT TO respond, because it includes something else appart from the JSON object:
+    "Here is the answer:
+    {
+        "explanation": "placeholder explanation"
+    }"
+    Do not mention the index of any index in your explanation.
 
 
     Use the following examples for reference:`
